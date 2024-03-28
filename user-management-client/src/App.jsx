ad3b3c4a86618b90561b8f3ed2616e399596d5b5
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-03-29 01:24:42 +0600

    connect server and client and print user in ui

--- user-management-client/src/App.jsx
+++ user-management-client/src/App.jsx
@@ -18,18 +18,39 @@ const handleAddUsers = event =>{
   // console.log(name, email);
   const user ={name, email};
   console.log(user);
+  fetch("http://localhost:5000/users", {
+    method:'POST',
+    headers:{
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+  .then(res =>res.json())
+  .then(data =>{
+    // 
+    const newUser =[...users, data]
+    setUsers(newUser);
+    form.reset();
+  })
 }
   return (
     <>
       <h1>User Management system client site</h1>
       <h3>Total Number of Users is {users.length}</h3>
       <form onSubmit={handleAddUsers} action="">
-        <input type="text" name="name" id="" placeholder='Enter your name' />
+        
+       <input type="text" name="name" id="" placeholder='Enter your name' />
         <br />
         <input type="email" name="email" id="" placeholder='Enter your email'/>
         <br />
         <input type="submit" value="Add User" />
       </form>
+      {
+        users.map(user =><p key={user.id}>
+        {user.id}. Name:{user.name}   email: {user.email} 
+        
+        </p>)
+      }
      
     </>
   )
